be25c4d43f4a4c72d3950c85fdd3288e675afd06
Felipe Sakaguti <felipesakaguti@hotmail.com>
2020-08-20 21:47:02 -0300

    Alterado conexão com Backend

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-    // baseURL: 'https://backendtdt-responsive-ardvark-nv.mybluemix.net/',
-    baseURL: 'http://192.168.15.66:3333/'
+     baseURL: 'https://localhost:3333/',
+    // baseURL: 'http://backendtdt-responsive-ardvark-nv.mybluemix.net/'
 })
 
 export default api;
